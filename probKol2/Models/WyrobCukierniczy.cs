e97bb67999c3e296e23b044eaae02c2ec698664e
magladko <kornel.marek@gmail.com>
2022-06-22 22:19:06 +0200

    5.0

--- probKol2/Models/WyrobCukierniczy.cs
+++ probKol2/Models/WyrobCukierniczy.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace probKol.Models
 {
     public class WyrobCukierniczy
--- probKol2/Models/Zamowienie.cs
+++ probKol2/Models/Zamowienie.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace probKol.Models
 {
     public class Zamowienie
