103c4e214f26feb8d887731ee64256b0c43e62b0
magladko <kornel.marek@gmail.com>
2022-06-22 22:21:06 +0200

    com

--- probKol2/Models/Zamowienie.cs
+++ probKol2/Models/Zamowienie.cs
@@ -8,7 +8,7 @@ namespace probKol.Models
         public int IdZamowienia { get; set; }
         public DateTime DataPrzyjecia { get; set; }
         public DateTime DataRealizacji { get; set; }
-        public string? Uwagi { get; set; }
+        public string Uwagi { get; set; }
         public int IdKlient { get; set; }
         public int IdPracownik { get; set; }
         public virtual Klient Klient { get; set; }
